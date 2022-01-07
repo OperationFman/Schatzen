371c3ffe2a6bd1b38416f22508e20e4bc6174138
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-01-07 20:36:06 +1100

    Add aws config

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { region, secretAccessKey, accessKeyId } from "../dynamo/config";
+
+import * as AWS from "aws-sdk";
+import { ConfigurationOptions } from "aws-sdk";
+
+const configuration: ConfigurationOptions = {
+  region: region,
+  secretAccessKey: secretAccessKey,
+  accessKeyId: accessKeyId,
+};
+
+AWS.config.update(configuration);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
