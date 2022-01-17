5398da8b267e0b99c871ccf7d703dc7c031c3792
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-01-17 12:53:18 +1100

    Added basic home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import "./App.css";
-import { fetchAllTableData } from "./db/Dynamo";
-import {
-  addNewUser,
-  updatePoint,
-  resetAllPoints,
-  wipeAllData,
-  updateUserAndPoint,
-} from "./db/DataService";
+import { Home } from "./Home/Home";
 
 function App() {
-  const getData = async () => {
-    const result = await updatePoint("BATMAN", 123);
-    console.log(result)
-  };
-
   return (
     <div className="App">
-      <button onClick={getData}>Fetch</button>
+      <Home />
     </div>
   );
 }
