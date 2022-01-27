6c59cc0347a2f91be99a2a9463bc8280d86490e2
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-01-27 12:40:28 +1100

    Fix alert message to display reliably

--- frontend/src/Alert/AlertController.tsx
+++ frontend/src/Alert/AlertController.tsx
@@ -1,49 +1,67 @@
 import { AlertMessage } from "./AlertMessage";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
+import Button from "@mui/material/Button";
 
-type Colors = "error" | "warning" | "info" | "success"
+type Colors = "error" | "warning" | "info" | "success";
 
 export type AlertProps = {
-    message: string;
-    color: Colors;
-}
+  message: string;
+  color: Colors;
+};
 
 export const AlertController = () => {
-    const [messageList, setMessageList] = useState([])
-    
-    const [messageDisplayed, setMessageDisplayed] = useState(false);
-    const [alertText, setAlertText] = useState("You shouldn't be able to see this");
-    const [alertColor, setAlertColor] = useState<Colors>("info");
+  const [messageList, setMessageList] = useState<AlertProps[]>([]);
+  const [messageDisplayed, setMessageDisplayed] = useState(false);
+  const [alertText, setAlertText] = useState("");
+  const [alertColor, setAlertColor] = useState<Colors>("info");
 
-    useEffect(() => {
-        if (messageList.length > 0) {
-            const displayMessage = (message: string, color: Colors) => {
-                setAlertText(message);
-                setAlertColor(color);
-                setMessageDisplayed(true);
-            }
+  const dataLoaded = useRef(false);
+  useEffect(() => {
+    if (messageList.length > 0 && !dataLoaded.current) {
+      const { message, color } = messageList[0];
 
-            const {message, color} = messageList[0]
-            displayMessage(message, color)
-            setTimeout(() => { setMessageDisplayed(false) }, 5000)
-            setMessageList(messageList.slice(1));
-        }
-    }, [messageList])
+      const displayMessage = (message: string, color: Colors) => {
+        setAlertText(message);
+        setAlertColor(color);
+        setMessageDisplayed(true);
+      };
 
-    return (
-        <>
-            <AlertMessage visible={messageDisplayed} message={alertText} color={alertColor}/>
-        </>
-    )
-}
+      const removeMessage = () => {
+        setMessageDisplayed(false);
+        setTimeout(() => {
+          setMessageList(messageList.slice(1));
+        }, 500);
+      };
 
-// What was I doing?
-    // Trying to test the above useEffect by adding a button that will push a new message/color to the messageList
-    // This SHOULD trigger the useEffect
-    // It should make AlertMessage appear, wait 5 seconds and hide it
-    // Then the messageList should be empty and the useEffect will stop (So the Alert will stay disappeared)
-    // Clicking the button multiple times should trigger the useEffect to keep running until the array is empty
-    // It would be great if displayMessage didn't need to be inside the useEffect
-    // Finally, add tests
+      displayMessage(message, color);
+      setTimeout(() => {
+        removeMessage();
+      }, 2000);
+    }
+  }, [messageList]);
+
+  const appendMessageList = (message: string, color: Colors) => {
+    setMessageList((prevState) => [...prevState, { message, color }]);
+  };
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          appendMessageList("Submitted your score", "success");
+        }}
+      >
+        Hurry Up!
+      </Button>
+      <AlertMessage
+        visible={messageDisplayed}
+        message={alertText}
+        color={alertColor}
+      />
+    </>
+  );
+};
+
+// Add tests
 
 // Once this works, setup the webSocket to fill the array instead of the button
